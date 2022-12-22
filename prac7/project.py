c0566f0b6723c628a1d905ceb0578a8ebb73c1c0
DonglinLiJCU <donglin.li1@my.jcu.edu.au>
2022-12-22 18:17:43 +0800

    All finished

--- prac7/project.py
+++ prac7/project.py
@@ -14,12 +14,28 @@ class Project:
         self.priority = priority
         self.cost_estimate = cost_estimate
         self.completion_percentage = completion_percentage
+        self.compare_mode = 0
 
     def __repr__(self):
         return f"{self.name}, start: {self.start_date.strftime('%d/%m/%Y')}, priority {self.priority}, estimate: ${self.cost_estimate:.2f}, completion: {self.completion_percentage}%"
 
     def __lt__(self, other):
-        return self.priority < other.priority
+        if self.compare_mode == 0:
+            return self.priority < other.priority
+        elif self.compare_mode == 1:
+            return self.start_date < other.start_date
+
+    def set_compare_mode(self, mode):
+        self.compare_mode = mode
+
+    def update_completion(self, percentage):
+        self.completion_percentage = percentage
+
+    def update_priority(self, priority):
+        self.priority = priority
 
     def is_completed(self):
         return self.completion_percentage == 100
+
+    def file_str(self):
+        return f"{self.name}\t{self.start_date.strftime('%d/%m/%Y')}\t{self.priority}\t{self.cost_estimate}\t{self.completion_percentage}"
--- prac7/project_management.py
+++ prac7/project_management.py
@@ -1,7 +1,7 @@
 """
 Project Management
-Estimate: 20 minutes
-Actual:    minutes
+Estimate: 40 minutes
+Actual:   45 minutes
 """
 
 from prac7.project import Project
@@ -9,6 +9,7 @@ import datetime
 
 
 def main():
+    file_name = "projects.txt"
     projects = load_projects()
     show_menu()
     choices = ['L', 'S', 'D', 'F', 'A', 'U', 'Q']
@@ -26,8 +27,85 @@ def main():
                 for completed_project in projects_tuple[1]:
                     print(completed_project)
                 print()
+
+            elif choice.upper() == 'L':
+                last_name = file_name
+                try:
+                    file_name = input("Enter file name: ")
+                    projects = load_projects(file_name)
+                except FileNotFoundError:
+                    print("File not found.")
+                    file_name = last_name
+                except:
+                    print("Error loading file.")
+                    file_name = last_name
+                print()
+
+            elif choice.upper() == 'S':
+                out_file_name = input("Enter file name: ")
+                save_projects(projects, out_file_name)
+
+            elif choice.upper() == 'F':
+                try:
+                    start_date = input("Enter start date (dd/mm/yyyy): ")
+                    start_date = datetime.datetime.strptime(start_date, "%d/%m/%Y").date()
+                    tmp_projects = filter_projects(projects, start_date)
+                    for project in tmp_projects:
+                        print(project)
+                except ValueError:
+                    print("Invalid date.")
+
+            elif choice.upper() == 'A':
+                print("Let's add a new project")
+                try:
+                    name = input("Name: ")
+                    start_date = input("Start date (dd/mm/yyyy): ")
+                    start_date = datetime.datetime.strptime(start_date, "%d/%m/%Y").date()
+                    priority = int(input("Priority: "))
+                    cost_estimate = float(input("Cost estimate: $"))
+                    completion_percentage = int(input("Percent complete: "))
+                    while not 0 <= completion_percentage <= 100:
+                        print("Invalid percentage")
+                        completion_percentage = int(input("Percent complete: "))
+                    projects = add_project(projects, name, start_date, priority, cost_estimate, completion_percentage)
+                except ValueError:
+                    print("Invalid input.")
+
+            elif choice.upper() == 'U':
+                for i in range(len(projects)):
+                    print(f"{i} {projects[i]}")
+                choice = int(input("Project choice: "))
+                while not 0 <= choice <= len(projects) - 1:
+                    print("Invalid choice")
+                    choice = int(input("Project choice: "))
+                print(projects[choice])
+                new_percentage = input("New percentage: ")
+                if new_percentage != '':
+                    try:
+                        new_percentage = int(new_percentage)
+                        while not 0 <= new_percentage <= 100:
+                            print("Invalid percentage")
+                            new_percentage = int(input("New percentage: "))
+                    except ValueError:
+                        print("Invalid input.")
+                else:
+                    new_percentage = projects[choice].completion_percentage
+                new_priority = input("New priority: ")
+                if new_priority != '':
+                    try:
+                        new_priority = int(new_priority)
+                        while new_priority < 0:
+                            print("Invalid priority")
+                            new_percentage = int(input("New priority: "))
+                    except ValueError:
+                        print("Invalid input.")
+                else:
+                    new_priority = projects[choice].priority
+                projects = update_project(projects,choice, new_percentage, new_priority)
+
         show_menu()
         choice = input(">>> ")
+    save_projects(projects)
     print("Thank you for using custom-built project management software.")
 
 
@@ -59,6 +137,14 @@ def load_projects(name="projects.txt"):
     return projects
 
 
+def save_projects(projects, out_file_name="projects.txt"):
+    out_file = open(out_file_name, "w")
+    out_file.write("Name\tStart Date\tPriority\tCost Estimate\tCompletion Percentage\n")
+    for project in projects:
+        out_file.write(project.file_str() + "\n")
+    out_file.close()
+
+
 def display_projects(projects):
     incomplete = []
     completed = []
@@ -70,6 +156,30 @@ def display_projects(projects):
     completed.sort()
     incomplete.sort()
     return incomplete, completed
+
+
+def filter_projects(projects, start_date: datetime):
+    tmp_projects = []
+    for project in projects:
+        if project.start_date >= start_date:
+            tmp_project = Project(project.name, project.start_date, project.priority, project.cost_estimate,
+                                  project.completion_percentage)
+            tmp_project.set_compare_mode(1)
+            tmp_projects.append(tmp_project)
+    tmp_projects.sort()
+    return tmp_projects
+
+
+def add_project(projects, name, start_date, priority, cost_estimate, completion_percentage):
+    project = Project(name, start_date, priority, cost_estimate, completion_percentage)
+    projects.append(project)
+    return projects
+
+
+def update_project(projects, choice, new_percentage, new_priority):
+    projects[choice].update_completion(new_percentage)
+    projects[choice].update_priority(new_priority)
+    return projects
 
 
 main()
